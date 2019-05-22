dff652732ad2a360ebe31cf2a91095604753ecd3
carolineplin <caroline.plin1@gmail.com>
2019-05-22 17:02:31 +0200

    restaurant

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Dishoom',
+    address:      '7 Boundary St, London E2 7JE',
+    category:     'french',
+  },
+  {
+    name:         'Pizza East',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    category:     'italian',
+  },
+   {
+    name:         'Pizza Pino',
+    address:      '189 Rue de rome',
+    category:     'italian',
+  },
+  {
+    name:         'Japon',
+    address:      '10 rue paradis',
+    category:     'japanese',
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
